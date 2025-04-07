ed4ce9fa9ccfd3c286b0a0b46220b45e4a3e5c8b
anndreipopa <andreipopa7804@gmail.com>
2025-04-07 22:43:08 +0300

    adding all my existing files

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  const isExpanded = navLinks.classList.contains('active');
+  hamburger.setAttribute('aria-expanded', isExpanded);
+});
+
+// Optional: Închide meniul când se dă click pe un link
+const links = document.querySelectorAll('.nav-links a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        if (navLinks.classList.contains('active')) {
+            navLinks.classList.remove('active');
+            hamburger.classList.remove('active');
+            hamburger.setAttribute('aria-expanded', 'false');
+        }
+    });
+});
